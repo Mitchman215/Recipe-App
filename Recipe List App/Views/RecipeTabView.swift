055613b7bea418561cfd908dd6313af715f517a7
Mitchell Salomon <mitchell_salomon@brown.edu>
2021-06-28 13:01:19 -0400

    Test commit

---
+++ Recipe List App/Views/RecipeTabView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeTabView.swift
+//  Recipe List App
+//
+//  Created by Mitchell Salomon on 6/28/21.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}

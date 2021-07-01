f6eeb8eec582054fc4b7c2a6a7a32fb6145d524a
Mitchell Salomon <mitchell_salomon@brown.edu>
2021-07-01 11:58:45 -0400

    - Add Rational.swift for its greatestCommonDivisor Method - Implement getPortion method to calculate the correct display of ingredient portions - Add serving size picker to RecipeDetailView to allow users to select desired serving size - Add state var selectedServingSize to keep track of user's selection

--- Recipe List App/ViewModels/RecipeModel.swift
+++ Recipe List App/ViewModels/RecipeModel.swift
@@ -17,4 +17,61 @@ class RecipeModel: ObservableObject {
         self.recipes = DataService.getLocalData()
     }
     
+    static func getPortion(ingredient:Ingredient, recipeServings:Int, targetServings:Int) -> String {
+        var portion = ""
+        var numerator = ingredient.num ?? 1
+        var denominator = ingredient.denom ?? 1
+        var wholePortions = 0
+        
+        if ingredient.num != nil {
+            // Get a single serving size by multiplying denom by recipe servings
+            denominator *= recipeServings
+            
+            // Get target portion by multiplying numerator by target servings
+            numerator *= targetServings
+            
+            // Reduce fraction by greatest common divisor
+            let divisor = Rational.greatestCommonDivisor(numerator, denominator)
+            numerator /= divisor
+            denominator /= divisor
+            
+            // Get the whole portions if numerator > denom
+            if numerator >= denominator {
+                wholePortions = numerator / denominator
+                numerator = numerator % denominator
+                portion += String(wholePortions)
+            }
+            
+            // Express the remainder as a fraction
+            if numerator > 0 {
+                portion += wholePortions > 0 ? " " : ""
+                portion += "\(numerator)/\(denominator)"
+            }
+        }
+        
+        if var unit = ingredient.unit {
+            
+            // if we need to pluralize
+            if wholePortions > 1 {
+                // calculate appropriate suffix
+                if unit.suffix(2) == "ch" {
+                    unit += "es"
+                } else if unit.suffix(1) == "f" {
+                    unit = String(unit.dropLast())
+                    unit += "ves"
+                } else {
+                    unit += "s"
+                }
+            }
+            
+            
+            
+            portion += ingredient.num == nil && ingredient.denom == nil ? "" : " "
+
+            return portion + unit
+        }
+        
+        return portion
+    }
+    
 }
